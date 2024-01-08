5b560c0c574581445ccc956ede1c6281970a7e7f
Lee Sarah <imsolem1226@gmail.com>
2024-01-08 19:04:10 +0900

    Create air_quality_dag.py
    get data from api
    save data as .csv file format
    upload .csv file to gcs

---
+++ dags/air/air_quality_dag.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import (
+    LocalFilesystemToGCSOperator,
+)
+import requests
+import pandas as pd
+
+
+def fetch_data_and_save_csv():
+    url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
+    params = {
+        "serviceKey": "9IyndkiMrrzo5eLkP+I/sKhMYeg0jb8hNwqpdPHdeRKS5WuCsdT/bA8urOBesACx9E9cmdhLVs9sDvAFiyVlsA==",
+        "returnType": "json",
+        "numOfRows": "1000",
+        "pageNo": "1",
+        "sidoName": "전국",
+        "ver": "1.0",
+    }
+
+    response = requests.get(url, params=params)
+    json_data = response.json()["response"]["body"]["items"]
+    df = pd.DataFrame(json_data)
+    df.to_csv("dags/output.csv", index=False)
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1),
+    "retries": 1,
+}
+dag = DAG(
+    "upload_csv_to_gcs",
+    default_args=default_args,
+    catchup=False,
+    schedule="@daily",
+)  # Or set your schedule
+
+fetch_data_task = PythonOperator(
+    task_id="fetch_data_and_save_csv_task",
+    python_callable=fetch_data_and_save_csv,
+    dag=dag,
+)
+upload_operator = LocalFilesystemToGCSOperator(
+    task_id="upload_csv_to_gcs_task",
+    src="dags/output.csv",
+    dst="output.csv",
+    bucket="data-lake-storage",
+    gcp_conn_id="google_cloud_conn_id",  # The Conn Id from the Airflow connection setup
+    dag=dag,
+)
+fetch_data_task >> upload_operator
